eefa6ab3cee945935bdf42df0339e9054f07857d
Lavelle_Ali <lavelleali7@gmail.com>
2026-04-07 23:17:32 -0400

    Update: foundational code 18% complete

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,25 +1,45 @@
 import React from "react";
 import "./Cards.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Cards = () => {
   return (
     <div>
-      <div className="container">
-        <div className="row">
-          <div className="card__wrapper">
-            <h1 className="card__title">Question: 1</h1>
-            <h3 className="card__question">
-              What is the difference between let and var?
-            </h3>
-            <div className="answers">
-              <p><b>Scope:</b></p>
-              <p>"let" has a block scope</p>
-              <p>"var" has a function scope</p>
+      <html>
+        <div className="container">
+          <div className="row">
+            <div className="card__wrapper">
+              <h1 className="card__title">Question: 1</h1>
+              <h3 className="card__question">
+                What is the difference between let and var?
+              </h3>
+              <div className="answers">
+                <h4>
+                  <b>Scope:</b>
+                </h4>
+                <ul>
+                  <li>
+                    <p>"let" has a block scope</p>
+                  </li>
+                  <li>
+                    <p>"var" has a function scope</p>
+                  </li>
+                </ul>
+                <h4>
+                  <b>Hoisting:</b>
+                </h4>
+                <ul>
+                  <li>"var" gets hoisted</li>
+                  <li>"let" does not get hoisted</li>
+                </ul>
+              </div>
+              <div className="clickable__links">
+                <FontAwesomeIcon icon="fa-solid fa-rotate" spin />
+              </div>
             </div>
-            <div className="clickable__links"></div>
           </div>
         </div>
-      </div>
+      </html>
     </div>
   );
 };
