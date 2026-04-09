aefb5b3977e557681755773c9a7609f6adc8ac12
Lavelle_Ali <lavelleali7@gmail.com>
2026-04-08 21:23:15 -0400

    Update: created functionality in Cards component that allows toggling of visibility of answers upon clicking of solutions button

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the first flashcard question', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(
+    screen.getByText(/what is the difference between let and var\?/i)
+  ).toBeInTheDocument();
 });
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,45 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Cards.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faRotate } from "@fortawesome/free-solid-svg-icons";
 
 const Cards = () => {
+
+  const [answers, setAnswers] = useState(false);
+
+  function toggleAnswer() {
+    setAnswers((answers) => !answers )
+  }
+
+
   return (
     <>
-    <div className="container">
-      <div className="row">
-        <div className="card__wrapper">
-          <h1 className="card__title">Question: 1</h1>
-          <h3 className="card__question">
-            What is the difference between let and var?
-          </h3>
-          <div className="answers">
-            <h4>
-              <b>Scope:</b>
-            </h4>
-            <ul>
-              <li>
-                <p>"let" has a block scope</p>
-              </li>
-              <li>
-                <p>"var" has a function scope</p>
-              </li>
-            </ul>
-            <h4>
-              <b>Hoisting:</b>
-            </h4>
-            <ul>
-              <li>"var" gets hoisted</li>
-              <li>"let" does not get hoisted</li>
-            </ul>
-          </div>
-          <div className="clickable__links">
-            <FontAwesomeIcon icon={faRotate} spin />
+      <div className="container">
+        <div className="row">
+          <div className="card__wrapper">
+            <div className="bck__drp">
+              <h1 className="card__title">Question: 1</h1>
+              <h3 className="card__question">
+                What is the difference between let and var?
+              </h3>
+              <div className={`answers ${answers ? 'answers__visible' : ""}`}>
+                <h4>
+                  <b>Scope:</b>
+                </h4>
+                <ul className="list">
+                  <li>
+                    <p>"let" has a block scope</p>
+                  </li>
+                  <li>
+                    <p>"var" has a function scope</p>
+                  </li>
+                </ul>
+                <h4>
+                  <b>Hoisting:</b>
+                </h4>
+                <ul className="list">
+                  <li>"var" gets hoisted</li>
+                  <li>"let" does not get hoisted</li>
+                </ul>
+              </div>
+              
+            </div>
+
+            <div className="clickable__links">
+              <button className="card__nav">
+                <FontAwesomeIcon icon="fa-solid fa-angle-left" />
+                Previous
+              </button>
+              <button className="card__nav" onClick={toggleAnswer}>
+                {toggleAnswer ? answers : ""}
+                Solution
+                <FontAwesomeIcon icon={faRotate} spin />
+              </button>
+              <button className="card__nav">
+                Next
+                <FontAwesomeIcon icon="fa-solid fa-angle-right" />
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    
     </>
   );
 };
